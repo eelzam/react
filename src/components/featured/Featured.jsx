045b8eaad714e552e834eefcfdb6366279ded4c3
eladelzam <elad.elzam@fundbox.com>
2022-10-20 10:27:35 +0300

    fixing typo

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -18,7 +18,7 @@ let stories2 = [
     					  <h1> קונצרט</h1>
 
 						  <h2>
-						    הופעות בזא׳נר שלי
+						    הופעות בז׳אנר שלי
 							 </h2>
 						  <h3>
   						  כל מה שקורה בזמן אמת
@@ -28,74 +28,6 @@ let stories2 = [
 
 		}
 	}
-
-	/*,
-		{
-		content: ({ action, isPaused }) => {
-			return <div style={contentStyle}>
-
-                    <img className="mainimage" src="https://a.storyblok.com/f/178921/400x500/92620f3de3/function-uuid4-at-0x7fa853756b00.jpg" alt=""/>
-
-                      <div className="info">
-
-                        <span className="desc">
-
-
-                          מחכה לכם בהופעה מלאה🎤 עם כל השירים שאתם אוהבים
-                        #טיפה_של_אושר
-                        #בדקות_של_געגוע
-                        חפלה אש🔥גם נוסטליה
-                        אז אל תישארו בחוץ והזמינו מקומות כבר עכשיו!!
-
-                        #להזמנות_דרך_הווצאפ
-                        https://bit.ly/3PH01Wl
-                        הכניסה חינם, פתיחת דלתות בשעה 22:00
-                        🎼🎼🎼🎶🎵
-                        תל אביב, מקווה ישראל 25
-                        פינת מנחם בגין 16(גולדן לשעבר)
-                        להזמנות בטלפון - ☎️053-5555222☎️
-
-                        </span>
-
-                      </div></div>
-
-		}
-	},
-
-    {
-		content: ({ action, isPaused }) => {
-			return <div style={contentStyle}>
-
-                    <img className="mainimage" src="https://a.storyblok.com/f/178921/400x500/dd3a1f6336/function-uuid4-at-0x7f9f44c82ef0.jpg" alt=""/>
-
-                      <div className="info">
-
-                        <span className="desc">
-
-							  מחכה לכם בהופעה מלאה🎤 עם כל השירים שאתם אוהבים
-							#טיפה_של_אושר
-							#בדקות_של_געגוע
-							חפלה אש🔥גם נוסטליה
-							אז אל תישארו בחוץ והזמינו מקומות כבר עכשיו!!
-
-							#להזמנות_דרך_הווצאפ
-							https://bit.ly/3PH01Wl
-							הכניסה חינם, פתיחת דלתות בשעה 22:00
-							🎼🎼🎼🎶🎵
-							תל אביב, מקווה ישראל 25
-							פינת מנחם בגין 16(גולדן לשעבר)
-							להזמנות בטלפון - ☎️053-5555222☎️
-
-                        </span>
-
-                      </div></div>
-		}
-	},
-	{
-		url: 'https://picsum.photos/1080/1920',
-		seeMore: ({ close }) => <div style={{ maxWidth: '100%', height: '100%', padding: 40, background: 'white' }}><h2>Just checking the see more feature.</h2><p style={{ textDecoration: 'underline' }} onClick={close}>Go on, close this popup.</p></div>
-	}
-*/
 ]
 
 const contentStyle = {
@@ -109,8 +41,6 @@ const contentStyle = {
 const storyContentStyle = {
 	marginTop: '70px'
 }
-
-
 
 export default function Featured() {
 
